c318efd362a98fb2dc0ad930444423063a3673ed
MRHemel <hemelrahman80@gmail.com>
2023-05-27 17:40:06 +0600

    errorpage setup done

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,31 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Main from './layout/Main.jsx';
+import ErrorPage from './Components/ErrorPage.jsx';
+import Home from './Components/Home/Home.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
