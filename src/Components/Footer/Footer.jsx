a6de2eba4bea7a985062de375f702739f85f75c1
MRHemel <hemelrahman80@gmail.com>
2023-05-27 18:30:27 +0600

    navbar done

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import './Footer.css'
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+
 
 const Footer = () => {
     return (
-        <div>
-            <h2>this is footer</h2>
-        </div>
+        <footer className="footer">
+            <div className="footer-content">
+                <div className="footer-section">
+                    <h3>About Us</h3>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo dignissim ligula ac cursus.</p>
+                </div>
+                <div className="footer-section">
+                    <h3>Contact Us</h3>
+                    <p>123 Chef Street</p>
+                    <p>New York, NY 12345</p>
+                    <p>Email: info@chefwebsite.com</p>
+                    <p>Phone: (123) 456-7890</p>
+                </div>
+                <div className="footer-section">
+                    <h3>Follow Us</h3>
+                    <div className="social-icons flex flex-col">
+                        <Link className='flex text-xl'> <FaFacebook></FaFacebook> <small className='text-xl'>Facebook</small> </Link>
+                        <Link className='flex text-xl'> <FaInstagram></FaInstagram> <small className='text-xl'>Instagram</small> </Link>
+                        <Link className='flex text-xl'> <FaTwitter></FaTwitter> <small className='text-xl'>Twitter</small> </Link>
+
+
+                    </div>
+                </div>
+            </div>
+            <div className="footer-bottom">
+                <p>&copy; 2023 Chef Website. All rights reserved.</p>
+            </div>
+        </footer>
     );
 };
 
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,7 +3,24 @@ import React from 'react';
 const Header = () => {
     return (
         <div>
-            <h2>this is header </h2>
+            <div className="navbar bg-base-100">
+                <div className="flex-1">
+                    <a className="btn btn-ghost normal-case text-xl">Chef Recipe Fair</a>
+                </div>
+                <div className="flex-none">
+                    <ul className="menu menu-horizontal px-1">
+                        <li><a>Home</a></li>
+                        <li tabIndex={0}>
+                            <a>
+                                Blog
+
+                            </a>
+
+                        </li>
+                        <li><a>Profile</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
